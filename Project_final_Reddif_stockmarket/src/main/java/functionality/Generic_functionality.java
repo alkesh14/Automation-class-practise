7f786eae63afcb9efc74d891bbe2e30d126035dd
alkesh14 <155456612+alkesh14@users.noreply.github.com>
2024-01-08 09:33:29 +0530

    Final project automation

---
+++ Project_final_Reddif_stockmarket/src/main/java/functionality/Generic_functionality.java
@@ -0,0 +1,30 @@
+package functionality;
+
+public class Generic_functionality {
+
+	public void open_broweser(String browser) {
+
+	}
+
+	public void navigate(String url) {
+
+	}
+
+	public void click(String locator) {
+
+	}
+
+	public void get_text() { // no need for argument we will use it after click and other function to get
+								// text directly
+
+	}
+
+	public void select(String locator, String option) {
+
+	}
+
+	public void type(String locator, String Input_Text) { // to send text input
+
+	}
+
+}
---
+++ Project_final_Reddif_stockmarket/src/main/java/functionality/Validation_functionality.java
@@ -0,0 +1,17 @@
+package functionality;
+
+public class Validation_functionality {
+	
+	public void validate_title() {
+		
+	}
+	
+	public void validate_text() {
+		
+	}
+	
+	public void validate_element() {
+		
+	}
+
+}
